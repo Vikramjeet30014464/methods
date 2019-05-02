d2740ecaa31406e86fe3fd7644e46d7cdc4f4a40
Vikramjeet singh Singh <30014464@student.toiohomai.ac.nz>
2019-05-02 13:50:55 +1200

    msg

---
+++ method/method/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Q1*/
+            Console.WriteLine("This application will change the sign of a number...");
+            Console.WriteLine("Please enter your number: ");
+            Console.WriteLine($"The number with its sign changed would be: {ChangeSign(double.Parse(Console.ReadLine()))}");
+
+            Console.ReadLine();
+
+            /*Q2*/
+            Console.WriteLine("This application will convert miles to km's... ");
+            Console.Write("Please enter a distance in miles: ");
+            double miles = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{miles} = {Converter(miles)}KM's");
+        }
+        
+        /*Method for Q2*/
+        static double ChangeSign(double miles)
+        {
+            return miles * 0.62137;
+        }
+        /*Method for Q1*/
+        static double ChangeSign(double num)
+        {
+            return num * -1;
+        }
+        
+        //2*/
+          static void Main(string[] args)
+
+        {   double apple = 10;
+            Circumference(apple);
+        }
+        static double Circumference(double radius)
+        {
+            Maths.PI* radius;
+        }
+        
+        }
+    } 
+}
